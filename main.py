d5d31c9acdd9d656b0a9134efdc03b2bf52816d8
CodeCrafters <your-noreply-codecrafter.cuj@gmail.com@users.noreply.github.com>
2025-03-27 15:55:21 +0530

    text cleaner

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ import tensorflow.keras import datasets, layers, models
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.loads_data()
 training_images,  testing_images = testing_images / 255, testing_images / 255
 
-class _names = ['']
+class _names = ['Plane', 'Car', 'Bird', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+for i in range(16):
+    plt.subplot.(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+
+    plt.show()
