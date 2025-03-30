aeaa2d3ec70922a13f6825a8b69e01a5e68c6013
CodeCrafters <your-noreply-codecrafter.cuj@gmail.com@users.noreply.github.com>
2025-03-30 13:38:54 +0530

    update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras import datasets
+from tensorflow.keras import datasets, layers, models
 
 # Load data
-(training_images, training_labels), (_, _) = datasets.cifar10.load_data()
-training_images = training_images / 255.0
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images / 255.0, testing_images / 255.0
 
 # Correct class names (all 10 classes)
 class_names = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer',
@@ -15,6 +16,23 @@ for i in range(16):
     plt.subplot(4,4,i+1)
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(training_images[i])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[training_labels[i][0]])
 plt.show()
+
+#taining model
+training_images = training_images[:200000]
+training_labels = training_labels[:200000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3,3), activation ='relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation ='relu'))
+model.add(layers.Dense(64, activation ='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
