9627e3d13b765cf1d2b2bfb864ed047f7cab480a
CodeCrafters <your-noreply-codecrafter.cuj@gmail.com@users.noreply.github.com>
2025-03-30 12:28:15 +0530

    update

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import cv2 as cv
 import numpy as np
-import tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets
 
-(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.loads_data()
-training_images,  testing_images = testing_images / 255, testing_images / 255
+# Load data
+(training_images, training_labels), (_, _) = datasets.cifar10.load_data()
+training_images = training_images / 255.0
 
-class _names = ['Plane', 'Car', 'Bird', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+# Correct class names (all 10 classes)
+class_names = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer',
+               'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
 
 for i in range(16):
-    plt.subplot.(4,4,i+1)
+    plt.subplot(4,4,i+1)
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.imshow(training_images[i])
     plt.xlabel(class_names[training_labels[i][0]])
-
-    plt.show()
+plt.show()
